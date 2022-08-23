8d4f9df35150911544d530428d8955551795b5e2
Velfin Velasquez <velfinvelasquez@gmail.com>
2022-08-23 16:46:29 -0500

    Request maping

--- src/main/java/com/velfin/web/app/controllers/IndexController.java
+++ src/main/java/com/velfin/web/app/controllers/IndexController.java
@@ -3,11 +3,13 @@ package com.velfin.web.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
-	@GetMapping({ "/index", "/", "home" })
+	@GetMapping({ "/index", "/", "home", "" })
 	public String index(Model model) {
 		model.addAttribute("titulo", "Spring boot");
 		return "index";
