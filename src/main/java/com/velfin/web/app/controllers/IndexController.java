2df42d9a142763ad5930f3cb0ad61a3e16d9893d
Velfin Velasquez <velfinvelasquez@gmail.com>
2022-08-23 21:31:58 -0500

    modelos en spring mvc

--- src/main/java/com/velfin/web/app/controllers/IndexController.java
+++ src/main/java/com/velfin/web/app/controllers/IndexController.java
@@ -1,9 +1,10 @@
 package com.velfin.web.app.controllers;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.velfin.web.app.models.Usuario;
 
 @Controller
 @RequestMapping("/app")
@@ -13,5 +14,17 @@ public class IndexController {
 	public String index(Model model) {
 		model.addAttribute("titulo", "Spring boot");
 		return "index";
+	}
+	
+	@GetMapping({ "/perfil"})
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Velfin");
+		usuario.setApellido("Velasquez");
+		
+		model.addAttribute("nombre", usuario.getNombre());
+		model.addAttribute("apellido", usuario.getApellido());
+		model.addAttribute("titulo","Perfil");
+		return "perfil";
 	}
 }
